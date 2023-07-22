07de10d8d3099843207e2ab897437a71b03eb2aa
evaliu2002 <evaxinerliu@gmail.com>
2023-07-22 15:41:24 -0700

    creating divs to template and setup for file explorer and console or logs

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
 import React from "react";
 import AntimonyEditor from "./Editor/AntimonyEditor";
+import FileList from "./FileExplorer/FileList";
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <h1>The Official Antimony Web Code Editor</h1>
-      <div>
-        <AntimonyEditor />
+    <div id="app">
+      <h1 style={{ textAlign: 'center' }}>The Official Antimony Web Code Editor</h1>
+      <div
+        style={{
+          display: 'flex',
+          height: '100vh',
+          width: '100vw',
+          fontFamily: 'sans-serif',
+        }}
+      >
+        <div
+          style={{ 
+            width: 180,
+            borderRight: '1px solid rgba(0, 0, 0, .08)'
+          }}
+        >
+          
+        </div>
+        <div id="editor">
+          <FileList />
+          <AntimonyEditor />
+        </div>
       </div>
     </div>
   );
