1f772f921298c5dd1fc005b07d867ed3652bad37
evaliu2002 <evaxinerliu@gmail.com>
2023-07-27 13:31:15 -0700

    fixed syntax rules and added bracket colorization

--- src/languages/AntimonyLanguage.ts
+++ src/languages/AntimonyLanguage.ts
@@ -8,20 +8,22 @@ export const antimonyLanguage: monaco.languages.IMonarchLanguage = {
       [/\(|\)/, 'connected-parentheses'], // Rule for connected parentheses
       [/=>|->/, 'transform'],
       [/=|:=/, 'assign'],
-      ['\\-|\\+|\\*|\\/|\\^|\\;|at|in|import|has', 'operator'],
+      ['\\-|\\+|\\*|\\/|\\^|\\;|at|in|import|has', 'keywords-operator'],
       [
         /(?:identity|biological_entity_is|hasPart|part|isPartOf|parthood|isVersionOf|hypernym|hasVersion|version|isHomologTo|homolog|isDescribedBy|description|isEncodedBy|encoder|encodes|encodement|occursIn|container|hasProperty|property|isPropertyOf|propertyBearer|hasTaxon|taxon|sboTerm|model_entity_is|origin)/,
         'annotation'
       ],
-      [/(React|Removal)(.*?):/, 'react-remov'], // Add this rule for strings starting with "React" or "react" and ending with a colon
+      [/\b[a-zA-Z0-9_]+\:/, 'react-remov'], // Add this rule for strings starting with anything except for symbols and ending with a colon
       [/@?[a-zA-Z][\w$]*/, {
         cases: {
+          const: 'const',
+          unit: 'unit',
+          var: 'var',
+          species: 'species',
+          function: 'function',
           model: 'model',
           end: 'end',
-          species: 'species',
           compartment: 'compartment',
-          var: 'var',
-          const: 'const',
           '@default': 'other',
         },
       }],
