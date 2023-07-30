483aae2d7ceca29513ab241f55579ca3a61f6bc5
evaliu2002 <evaxinerliu@gmail.com>
2023-07-29 18:12:05 -0700

    progress with parsing and hover, note to improve code performance on parser

---
+++ src/languages/AntimonyParser.ts
@@ -0,0 +1,120 @@
+interface Compartment {
+    name: string;
+  }
+  
+  interface Species {
+    name: string;
+    compartment: string;
+  }
+  
+  interface Reaction {
+    name: string;
+    equation: string;
+  }
+  
+  interface Declaration {
+    name: string;
+    value: string | number;
+  }
+  
+  interface AntimonyModel {
+    compartments: Map<string, Compartment>;
+    species: Map<string, Species>;
+    reactions: Map<string, Reaction>;
+    declarations: Map<string, Declaration>;
+  }
+  
+  export function parseAntimonyModel(antimonyModel: string): AntimonyModel {
+    const lines = antimonyModel.split('\n');
+    const model: AntimonyModel = {
+      compartments: new Map(),
+      species: new Map(),
+      reactions: new Map(),
+      declarations: new Map(),
+    };
+  
+    let currentSection: 'compartments' | 'species' | 'reactions' | 'declarations' | null = null;
+    let lastCompartment: string | null = null;
+  
+    lines.forEach((line) => {
+      if (line.trim().startsWith('//')) {
+        // Skip comments
+        return;
+      }
+  
+      if (line.trim().length === 0) {
+        // Skip empty lines
+        return;
+      }
+  
+      if (line.trim().startsWith('model')) {
+        // Skip the model definition
+      }
+  
+      if (line.trim().startsWith('compartment')) {
+        currentSection = 'compartments';
+      }
+  
+      if (line.trim().startsWith('species')) {
+        currentSection = 'species';
+      }
+  
+      if (line.includes(':')) {
+        currentSection = 'reactions';
+      }
+  
+      if (line.trim().startsWith('var ') || line.trim().startsWith('const ')) {
+        currentSection = 'declarations';
+      }
+  
+      if (currentSection === 'compartments') {
+        const match = line.match(/compartment (.+);/);
+        if (match) {
+          const compartmentNames = match[1].split(',').map((name) => name.trim());
+          compartmentNames.forEach((compartmentName) => {
+            model.compartments.set(compartmentName, { name: compartmentName });
+            lastCompartment = compartmentName;
+          });
+        }
+      } else if (currentSection === 'species') {
+        const match = line.match(/species (.+)/);
+        if (match) {
+          const speciesLines = match[1].split(',').map((speciesLine) => speciesLine.trim());
+          speciesLines.forEach((speciesLine) => {
+            const speciesName = speciesLine.split(' ')[0];
+            const compartmentMatch = speciesLine.match(/in (.+)/);
+            if (compartmentMatch) {
+              const compartment = compartmentMatch[1].trim();
+              model.species.set(speciesName, { name: speciesName, compartment });
+            } else if (lastCompartment) {
+              model.species.set(speciesName, { name: speciesName, compartment: lastCompartment });
+            } else {
+              console.warn(
+                'Warning: Species found before any compartment declaration. Assigning to default compartment.'
+              );
+              model.species.set(speciesName, { name: speciesName, compartment: 'default' });
+            }
+          });
+        }
+      } else if (currentSection === 'reactions') {
+        const match = line.match(/(\S+): (.+);/);
+        if (match) {
+          const reactionName = match[1];
+          model.reactions.set(reactionName, {
+            name: reactionName,
+            equation: match[2],
+          });
+        }
+      } else if (currentSection === 'declarations') {
+        const match = line.match(/(var (.+)|const (.+))/);
+        if (match) {
+          const declarationNames = match[1].split(',').map((name) => name.trim().replace('const ', ''));
+          declarationNames.forEach((name) => {
+            model.declarations.set(name, { name: name, value: ''});
+          });
+        }
+      }
+    });
+  
+    return model;
+  }
