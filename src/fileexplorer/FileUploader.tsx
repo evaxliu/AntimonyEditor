068a0a2f84de0aba94d87c8a53b85eea8faf03f7
evaliu2002 <evaxinerliu@gmail.com>
2023-08-13 20:49:55 -0700

    progress in saving to client database multiple files

---
+++ src/fileexplorer/FileUploader.tsx
@@ -0,0 +1,105 @@
+import React, { useRef, useState, useEffect } from 'react';
+import { openDB } from 'idb';
+
+const FileUploader = () => {
+    const fileInputRef = useRef<HTMLInputElement>(null);
+    const [files, setFiles] = useState<any[]>([]);
+
+    useEffect(() => {
+        async function fetchFiles() {
+            try {
+                const db = await openDB('filesDatabase', 1, {
+                    upgrade(db) {
+                        if (!db.objectStoreNames.contains('files')) {
+                            const fileStore = db.createObjectStore('files', { autoIncrement: true });
+                            // Add any other initialization or migration steps here
+                        }
+                    },
+                });
+
+                const transaction = db.transaction('files', 'readonly');
+                const fileStore = transaction.objectStore('files');
+
+                const storedFiles = await fileStore.getAll();
+
+                // Sort the files in alphanumeric order by file name
+                storedFiles.sort((a, b) => a.data.name.localeCompare(b.data.name, 'en', { numeric: true }));
+
+                setFiles(storedFiles);
+            } catch (error) {
+                console.error('Error fetching files from IndexedDB: ', error);
+            }
+        }
+
+        fetchFiles();
+    }, []); // Run only once, when the component mounts
+
+    // Utility function to read file content asynchronously
+    const readFileContent = (file: File): Promise<string> => {
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.onload = () => {
+                const fileText = reader.result?.toString() || '';
+                console.log(fileText)
+                resolve(fileText);
+            };
+            reader.onerror = (error) => {
+                reject(error);
+            };
+            reader.readAsText(file);
+        });
+    };
+
+    const handleFileUpload = async (event: React.ChangeEvent<HTMLInputElement>) => {
+        const selectedFile = event.target.files && event.target.files[0];
+
+        if (selectedFile) {
+            try {
+                const fileContent = await readFileContent(selectedFile);
+
+                const db = await openDB('filesDatabase', 1, {
+                    upgrade(db) {
+                        if (!db.objectStoreNames.contains('files')) {
+                            db.createObjectStore('files', { autoIncrement: true });
+                        }
+                    },
+                });
+
+                const transaction = db.transaction('files', 'readwrite');
+                const fileStore = transaction.objectStore('files');
+
+                
+                // Store the file's text content in IndexedDB
+                await fileStore.add({ name: selectedFile.name, content: fileContent });
+                console.log('File content added to IndexedDB');
+
+                const storedFiles = await fileStore.getAll();
+
+                // Sort the files in alphanumeric order by file name
+                storedFiles.sort((a, b) => a.name.localeCompare(b.name, 'en', { numeric: true }));
+
+                setFiles(storedFiles);
+            } catch (error) {
+                console.error('Error adding file to IndexedDB: ', error);
+            }
+        }
+    };
+
+    return (
+        <div>
+            <input type="file" ref={fileInputRef} onChange={handleFileUpload} />
+            <div>
+                <h2>Uploaded Files:</h2>
+                <ul>
+                    {files.map((file: any) => (
+                        <button key={file.name} onClick={() => console.log('File clicked:', file)}>
+                            {file.name}
+                        </button>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default FileUploader;
